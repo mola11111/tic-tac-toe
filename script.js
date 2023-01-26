b8e58ef9f29184af1d6b5f8ba83bde1ebd5612fc
mola11111 <37241047+mola11111@users.noreply.github.com>
2023-01-26 19:53:40 +0900

    구조 변경

--- script.js
+++ script.js
@@ -1,19 +1,65 @@
 var cream = "O"
 var tic = [0,0,0,0,0,0,0,0,0];
+var game = true;
 function test(par){
     // console.log(par);
     // console.log(cream);
     
 
     a = document.getElementById(String(par));
-    if(tic[par]==0)
+    if(game)
     {
-        cream =-(cream=="O") ? "X" : "O";
-        a.innerText = cream;
-        tic[par] = cream;
-    }
+        if(tic[par]==0)
+        {
+            cream =-(cream=="O") ? "X" : "O";
+            a.innerText = cream;
+            tic[par] = cream;
+        }
+        else{
+            console.log("deadlock")
+        }
 
-    if(tic.indexOf(0) == -1){
-        console.log("end");
+        if(getfail()==0){
+            console.log(("O WIN"));
+            game = false;
+            
+        } 
+        else if(getfail()==1){
+            console.log(("X WIN"));
+            game = false;
+        } 
+
+        if(tic.indexOf(0) == -1){
+            console.log("end");
+            game = false;
+        }
+    }   
+}
+
+function getfail(){
+    if(
+        tic[3]==tic[4]==tic[5] == 'O' ||
+        tic[0]==tic[1]==tic[2] == 'O' ||
+        tic[6]==tic[7]==tic[8] == 'O' ||
+        tic[0]==tic[3]==tic[6] == 'O' ||
+        tic[1]==tic[4]==tic[7] == 'O' ||
+        tic[2]==tic[5]==tic[8] == 'O' ||
+        tic[0]==tic[4]==tic[8] == 'O' ||
+        tic[2]==tic[4]==tic[6] == 'O'
+    )
+    return 0;
+    else if(
+        tic[0]==tic[1]==tic[2] == 'X' ||
+        tic[3]==tic[4]==tic[5] == 'X' ||
+        tic[6]==tic[7]==tic[8] == 'X' ||
+        tic[0]==tic[3]==tic[6] == 'X' ||
+        tic[1]==tic[4]==tic[7] == 'X' ||
+        tic[2]==tic[5]==tic[8] == 'X' ||
+        tic[0]==tic[4]==tic[8] == 'X' ||
+        tic[2]==tic[4]==tic[6] == 'X'
+    )
+    return 1;
+    else{
+        return -1;
     }
 }
