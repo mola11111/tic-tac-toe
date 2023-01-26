0396a56e3d8e5dcfb883919585bebd00769ea02f
mola11111 <ehdgus5979@gmail.com>
2023-01-26 21:18:21 +0900

    승패 표시

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ var turn = 1
 var tic = [0,0,0,0,0,0,0,0,0];
 const winCase = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 var game = true;
+
+
 function test(par){
     if(!game) return;
     if(tic[par] != 0){
         return;
+    }
+    if(turn ==1){
+        document.getElementById("turn_X").style.opacity = 1;
+        document.getElementById("turn_O").style.opacity = 0.2;
+    }
+    else if(turn == 2){
+        document.getElementById("turn_X").style.opacity = 0.2;
+        document.getElementById("turn_O").style.opacity = 1;
     }
     a = document.getElementById(String(par));
     tic[par] = turn;
@@ -20,9 +30,22 @@ function test(par){
         a = getResult(i);
         if(a==1){
             console.log(1); game = false;
+            document.getElementById("turn_X").style.color = "red";
+            document.getElementById("turn_O").style.color = "green";
+            document.getElementById("turn_O").style.opacity = 1;
+            document.getElementById("turn_X").style.opacity = 1;
+            return;
         }
         else if(a==2){
             console.log(2); game = false;
+            document.getElementById("turn_O").style.color = "red";
+            document.getElementById("turn_X").style.color = "green";
+            document.getElementById("turn_O").style.opacity = 1;
+            document.getElementById("turn_X").style.opacity = 1;
+            return;
+        }
+        else if(tic.indexOf(0) == -1){
+            console.log("draw"); game = false; return;
         }
     }
 
@@ -42,4 +65,10 @@ function getResult(gameCase){
     else{
         return 0;
     }
+}
+
+function gameInit(){
+    tic = [0,0,0,0,0,0,0,0,0];
+    game = true;
+    document.getElementsByClassName("box").innerText = 0;
 }
