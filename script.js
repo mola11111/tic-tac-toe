9c14b7e83a6fc8e71047f380651f11317b737210
mola11111 <37241047+mola11111@users.noreply.github.com>
2023-02-09 01:24:40 +0900

    refactoring

--- script.js
+++ script.js
@@ -1,82 +1,59 @@
-var turn = 1
-var tic = [0,0,0,0,0,0,0,0,0];
-const winCase = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-var game = true;
-const winColor = "green"
+var turn = 1;
+var tic = Array(9).fill(0);
+const winCases = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+var gameOver = false;
+const winColor = "green";
 const loseColor = "red";
 const winLineColor = "purple";
 
-function test(par){
-    if(!game) return;
-    if(tic[par] != 0){
-        return;
-    }
-    if(turn ==1){
-        document.getElementById("turn_X").style.opacity = 1;
-        document.getElementById("turn_O").style.opacity = 0.2;
-    }
-    else if(turn == 2){
-        document.getElementById("turn_X").style.opacity = 0.2;
-        document.getElementById("turn_O").style.opacity = 1;
-    }
-    a = document.getElementById(String(par));
-    tic[par] = turn;
-    if(tic[par] == 1){
-        a.innerText = "O"; turn = 2;
-    }
-    else{
-        a.innerText = "X"; turn = 1;
-    }
-    
-    for(i of winCase){
-        a = getResult(i);
-        if(a==1){
-            console.log(1); game = false;
-            document.getElementById("turn_X").style.color = loseColor;
-            document.getElementById("turn_O").style.color = winColor;
-            document.getElementById("turn_O").style.opacity = 1;
-            document.getElementById("turn_X").style.opacity = 1;
-            for(j of i){
-                document.getElementById(String(j)).style.color = winLineColor;
-            }
-            return;
-        }
-        else if(a==2){
-            console.log(2); game = false;
-            document.getElementById("turn_O").style.color = loseColor;
-            document.getElementById("turn_X").style.color = winColor;
-            document.getElementById("turn_O").style.opacity = 1;
-            document.getElementById("turn_X").style.opacity = 1;
-            for(j of i){
-                document.getElementById(String(j)).style.color = winLineColor;
-            }
-            return;
-        }
-    }
-    if(tic.indexOf(0) == -1){
-        console.log("draw"); game = false;
-        document.getElementById("turn_O").style.color = "blue";
-        document.getElementById("turn_X").style.color = "blue";
-        document.getElementById("turn_O").style.opacity = 1;
-        document.getElementById("turn_X").style.opacity = 1;
-         return;
-    }
-
+function play(squareIdx) {
+  if (gameOver) return;
+  if (tic[squareIdx] != 0) return;
+  
+  let symbol = (turn === 1) ? "O" : "X";
+  tic[squareIdx] = turn;
+  
+  document.getElementById(String(squareIdx)).innerText = symbol;
+  turn = (turn === 1) ? 2 : 1;
+  toggleTurnDisplay();
+  
+  let result = checkResult();
+  if (result !== 0) {
+    gameOver = true;
+    endGame(result, symbol);
+  } else if (tic.indexOf(0) === -1) {
+    gameOver = true;
+    endGame(0);
+  }
 }
 
-function getResult(gameCase){
-    if(
-        tic[gameCase[0]] == 1 && tic[gameCase[1]] == 1 && tic[gameCase[2]] == 1
-    ){
-        return 1;
-    }
-    else if(
-        tic[gameCase[0]] == 2 && tic[gameCase[1]] == 2 && tic[gameCase[2]] == 2
-    ){
-        return 2;
-    }
-    else{
-        return 0;
-    }
+function toggleTurnDisplay() {
+  document.getElementById("turn_O").style.opacity = (turn === 1) ? 1 : 0.2;
+  document.getElementById("turn_X").style.opacity = (turn === 2) ? 1 : 0.2;
 }
 
+function checkResult() {
+  for (let i of winCases) {
+    if (tic[i[0]] === tic[i[1]] && tic[i[1]] === tic[i[2]]) {
+      return tic[i[0]];
+    }
+  }
+  return 0;
+}
+
+function endGame(result, symbol) {
+  if (result === 0) {
+    console.log("draw");
+    document.getElementById("turn_O").style.color = "blue";
+    document.getElementById("turn_X").style.color = "blue";
+  } else {
+    console.log(symbol === "O" ? 1 : 2);
+    document.getElementById(`turn_${symbol}`).style.color = winColor;
+    document.getElementById(`turn_${(symbol === "O") ? "X" : "O"}`).style.color = loseColor;
+    for (let j of i) {
+      document.getElementById(String(j)).style.color = winLineColor;
+    }
+  }
+  document.getElementById("turn_O").style.opacity = 1;
+  document.getElementById("turn_X").style.opacity = 1;
+}
